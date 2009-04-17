eef579a230fd929f6c530d2606837a0d7bf8baac
dimok321@hotmail.de <dimok321@hotmail.de@6224d900-cc1e-4992-856c-54780e90ce17>
2009-04-17 16:15:51 +0000

    fixes

---
+++ trunk/source/filelist.h
@@ -0,0 +1,140 @@
+/****************************************************************************
+ * libwiigui Template
+ * Tantric 2009
+ *
+ * imagelist.h
+ * Contains a list of all of the files in the images, fonts, sounds folders
+ ***************************************************************************/
+
+#ifndef _FILELIST_H_
+#define _FILELIST_H_
+
+#include <gccore.h>
+
+extern const u8		font_ttf[];
+extern const u32	font_ttf_size;
+
+extern const u8		bg_music_ogg[];
+extern const u32	bg_music_ogg_size;
+
+extern const u8		button_over_pcm[];
+extern const u32	button_over_pcm_size;
+
+extern const u8		button_click_pcm[];
+extern const u32	button_click_pcm_size;
+
+extern const u8     background_png[];
+extern const u32    background_png_size;
+
+extern const u8     bg_options_settings_png[];
+extern const u32    bg_options_settings_png_size;
+
+extern const u8     settings_background_png[];
+extern const u32    settings_background_png_size;
+
+extern const u8     nocover_png[];
+extern const u32    nocover_png_size;
+
+extern const u8     button_install_png[];
+extern const u32    button_install_png_size;
+
+extern const u8     button_install_over_png[];
+extern const u32    button_install_over_png_size;
+
+extern const u8		button_png[];
+extern const u32	button_png_size;
+
+extern const u8		button_over_png[];
+extern const u32	button_over_png_size;
+
+extern const u8		menu_button_png[];
+extern const u32	menu_button_size;
+
+extern const u8		menu_button_over_png[];
+extern const u32	menu_button_over_size;
+
+extern const u8		settings_button_png[];
+extern const u32	settings_button_size;
+
+extern const u8		settings_button_over_png[];
+extern const u32	settings_button_over_size;
+
+extern const u8		button_large_png[];
+extern const u32	button_large_png_size;
+
+extern const u8		button_large_over_png[];
+extern const u32	button_large_over_png_size;
+
+extern const u8		wiimote_poweroff_png[];
+extern const u32	wiimote_poweroff_png_size;
+
+extern const u8		dialogue_box_png[];
+extern const u32	dialogue_box_png_size;
+
+extern const u8		wiimote_poweroff_over_png[];
+extern const u32	wiimote_poweroff_over_png_size;
+
+extern const u8		bg_options_png[];
+extern const u32	bg_options_png_size;
+
+extern const u8		bg_options_entry_png[];
+extern const u32	bg_options_entry_png_size;
+
+extern const u8		scrollbar_png[];
+extern const u32	scrollbar_png_size;
+
+extern const u8		scrollbar_arrowup_png[];
+extern const u32	scrollbar_arrowup_png_size;
+
+extern const u8		scrollbar_arrowup_over_png[];
+extern const u32	scrollbar_arrowup_over_png_size;
+
+extern const u8		scrollbar_arrowdown_png[];
+extern const u32	scrollbar_arrowdown_png_size;
+
+extern const u8		scrollbar_arrowdown_over_png[];
+extern const u32	scrollbar_arrowdown_over_png_size;
+
+extern const u8		scrollbar_box_png[];
+extern const u32	scrollbar_box_png_size;
+
+extern const u8		scrollbar_box_over_png[];
+extern const u32	scrollbar_box_over_png_size;
+
+extern const u8		progressbar_png[];
+extern const u32	progressbar_png_size;
+
+extern const u8		progressbar_empty_png[];
+extern const u32	progressbar_empty_png_size;
+
+extern const u8		progressbar_outline_png[];
+extern const u32	progressbar_outline_png_size;
+
+extern const u8		throbber_png[];
+extern const u32	throbber_png_size;
+
+extern const u8		player1_point_png[];
+extern const u32	player1_point_png_size;
+
+extern const u8		player2_point_png[];
+extern const u32	player2_point_png_size;
+
+extern const u8		player3_point_png[];
+extern const u32	player3_point_png_size;
+
+extern const u8		player4_point_png[];
+extern const u32	player4_point_png_size;
+
+extern const u8		player1_grab_png[];
+extern const u32	player1_grab_png_size;
+
+extern const u8		player2_grab_png[];
+extern const u32	player2_grab_png_size;
+
+extern const u8		player3_grab_png[];
+extern const u32	player3_grab_png_size;
+
+extern const u8		player4_grab_png[];
+extern const u32	player4_grab_png_size;
+
+#endif
